3d6b0fd5643e3a9ee3143df47e6246ea36e0df7f
dreine-dev <dreineantonio@hotmail.com>
2022-05-03 10:58:29 -0300

    Implementação da interface

---
+++ src/entities/Product.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void price(double price) {
+		this.price = price;
+	}
+
+}
